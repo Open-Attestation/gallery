4ce1e05c232e5b04dd28e912b77ae3b0dbbb3a1a
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-02-03 14:30:52 +0800

    feat: added header picture

--- src/components/headerPicture.tsx
+++ src/components/headerPicture.tsx
@@ -3,13 +3,13 @@ import styled from "@emotion/styled";
 import ClipArt from "../resources/clip-uploading.png";
 
 const HeaderTextOne = styled.div`
-  line-height: 5.5rem;
+  line-height: 4.8rem;
   letter-spacing: -0.02em;
   color: #2c3549;
 `;
 
 const HeaderTextTwo = styled.div`
-  line-height: 1.75rem;
+  line-height: 1.8rem;
 `;
 
 const ImgContainer = styled.img`
@@ -22,7 +22,7 @@ export const HeaderPicture: React.FunctionComponent = () => {
     <div className="flex flex-row justify-between px-56">
       <div className="mt-12">
         <HeaderTextOne className="font-semibold text-6xl">Gallery</HeaderTextOne>
-        <HeaderTextTwo className="font-light text-2xl">
+        <HeaderTextTwo className="font-light text-2xl mt-4">
           Explore OpenAttestation documents
           <br />
           and create yours today
