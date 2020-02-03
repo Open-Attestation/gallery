a3ed930ab45e1c16e91ca0daba41be20c1ab3854
Nebulis <nebounet@gmail.com>
2020-02-03 11:57:53 +0800

    chore: add type def for svg/png/jpg

---
+++ src/@types/declaration.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-default-export */
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
