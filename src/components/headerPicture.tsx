4cd1df9b22c2fc9f6bf9fb0c6e1a7e0b8151583c
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-02-03 11:08:02 +0800

    feat: added header picture

--- src/components/headerPicture.tsx
+++ src/components/headerPicture.tsx
@@ -1,11 +1,44 @@
 import React from "react";
-import "../app";
 import styled from "@emotion/styled";
+import Clipart from "../resources/clip-uploading.svg";
 
 const HeaderPictureContainer = styled.div`
   height: 19.5rem;
 `;
 
+const HeaderTextOne = styled.p`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 64px;
+  line-height: 77px;
+  letter-spacing: -0.02em;
+  color: #2c3549;
+`;
+
+const HeaderTextTwo = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 30px;
+`;
+
+const ImgContainer = styled.img`
+  padding-left: 14rem;
+  height: 310px;
+  object-fit: cover;
+`;
+
 export const HeaderPicture: React.FunctionComponent = () => {
-  return <HeaderPictureContainer className="test header-pic-main">test</HeaderPictureContainer>;
+  return (
+    <HeaderPictureContainer className="flex flex-row justify-between px-56">
+      <div className="mt-16">
+        <HeaderTextOne>Gallery</HeaderTextOne>
+        <HeaderTextTwo>Explore OpenAttestation documents</HeaderTextTwo>
+        <HeaderTextTwo>and create yours today</HeaderTextTwo>
+      </div>
+      <div>
+        <ImgContainer src={Clipart} alt="Uploading" />
+      </div>
+    </HeaderPictureContainer>
+  );
 };
