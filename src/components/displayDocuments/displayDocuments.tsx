871ab94d8576f18ec3ee578ddfd36698df2faca2
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-02-06 10:44:41 +0800

    feat: added qr render

--- src/components/displayDocuments/displayDocuments.tsx
+++ src/components/displayDocuments/displayDocuments.tsx
@@ -18,7 +18,7 @@ export const DisplayDocuments: React.FunctionComponent = () => {
       <SectionContainer>
         <DocumentCard
           title="OpenCerts Demo"
-          url="https://action.openattestation.com/?q=%7B%22type%22:%22DOCUMENT%22,%22payload%22:%7B%22uri%22:%22https://api.myjson.com/bins/1a9acm%22,%22key%22:%221b8c334a38f9ff96108303a4ba0cc592f1559eb24f5b48b70c9300c60a34d5e9%22,%22permittedAction%22:%5B%22STORE%22%5D,%22redirect%22:%22https://dev.opencerts.io%22%7D%7D"
+          url="https://action.openattestation.com?q=%7B%22type%22:%22DOCUMENT%22,%22payload%22:%7B%22uri%22:%22https://api.myjson.com/bins/1a9acm%22,%22key%22:%221b8c334a38f9ff96108303a4ba0cc592f1559eb24f5b48b70c9300c60a34d5e9%22,%22permittedActions%22:%5B%22STORE%22%5D,%22redirect%22:%22https://dev.opencerts.io%22%7D%7D"
           image={OpencertsDemoImage}
         />
         <DocumentCard title="Degree Certificate with Transcript" url="" />
