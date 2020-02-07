68a1d25edb3775789cb9213ef8aa3f2e7b9f4b91
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2020-02-07 15:35:13 +0800

    Feat/render qr (#8)

---
+++ src/components/displayDocuments/qrCodeButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { AiOutlineQrcode } from "react-icons/ai";
+
+const QrCodeButtonContainer = styled.div`
+  background: #feeee7;
+  border-radius: 12px 0px 4px;
+  width: 48px;
+  height: 48px;
+`;
+
+interface QrCodeProps {
+  handleClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+export const QrCodeButton: React.FunctionComponent<QrCodeProps> = ({ handleClick }) => {
+  return (
+    <QrCodeButtonContainer onClick={handleClick} className="absolute bottom-0 right-0 flex justify-center">
+      <AiOutlineQrcode size={20} className="self-center" />
+    </QrCodeButtonContainer>
+  );
+};
--- src/opencerts.tsx
+++ src/opencerts.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "./common/navbar/navbar";
+import { Navbar } from "./components/navbar/navbar";
 import { HeaderPicture } from "./components/headerPicture";
 import { MenuBar } from "./components/menuBar";
 import { DisplayDocuments } from "./components/displayDocuments/displayDocuments";
