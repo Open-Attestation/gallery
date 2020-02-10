e9f8bcc4df7431fbb519d000bc91c9ad0518eeae
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2020-02-10 15:48:19 +0800

    added responsive navbar (#9)

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,6 +4,11 @@ import styled from "@emotion/styled";
 
 const NavbarContainer = styled.div`
   background-color: #2c3549;
+  min-width: 1000px;
+`;
+
+const InnerContainer = styled.div`
+  min-width: 600px;
 `;
 
 export const Navbar: React.FunctionComponent = () => {
@@ -12,17 +17,19 @@ export const Navbar: React.FunctionComponent = () => {
   };
 
   return (
-    <NavbarContainer className="flex flex-row justify-between px-56 p-3 text-white">
-      <div className="flex self-center">
-        Open
-        <span className="font-bold">Attestation</span>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <a href="#" className="mr-5">
-          About
-        </a>
-        <UploadButton onClick={handleUpload}>Upload</UploadButton>
-      </div>
+    <NavbarContainer className="flex flex-row justify-center py-3 text-white">
+      <InnerContainer className="flex flex-row flex-grow flex-shrink justify-between mx-56">
+        <div className="flex self-center">
+          Open
+          <span className="font-bold">Attestation</span>
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <a href="#" className="mr-5">
+            About
+          </a>
+          <UploadButton onClick={handleUpload}>Upload</UploadButton>
+        </div>
+      </InnerContainer>
     </NavbarContainer>
   );
 };
