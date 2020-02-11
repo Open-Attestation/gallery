a92fac732fe3bcc3384b4c1422f597e27b788cd2
Sim Boon Long <simboonlong@gmail.com>
2020-02-11 15:15:41 +0800

    fix: menuBar casing

--- src/opencerts.tsx
+++ src/opencerts.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar } from "./components/navbar/navbar";
 import { HeaderPicture } from "./components/headerPicture";
-import { MenuBar } from "./components/MenuBar";
+import { MenuBar } from "./components/menuBar";
 import { DisplayDocuments } from "./components/displayDocuments/displayDocuments";
 
 export const OpencertsGallery: React.FunctionComponent = () => {
