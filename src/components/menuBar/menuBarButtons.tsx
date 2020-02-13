b7efd76704a221bdee8427917ac7a437c78495e0
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-02-13 18:03:14 +0800

    feat: added click to buttons

--- src/components/menuBar/menuBarButtons.tsx
+++ src/components/menuBar/menuBarButtons.tsx
@@ -2,59 +2,46 @@ import React from "react";
 import styled from "@emotion/styled";
 import { Tag } from "../displayDocuments/documents";
 
-const ButtonContainer = styled.button`
-  line-height: 17px;
-  letter-spacing: -0.02em;
-  padding: 6px 12px;
-  border-radius: 0.5rem;
-`;
+const Button = (props: { title: string; selected: boolean }): JSX.Element => {
+  const { title, selected } = props;
+  const ButtonContainer = styled.button`
+    line-height: 17px;
+    letter-spacing: -0.02em;
+    padding: 6px 12px;
+    border-radius: 0.5rem;
+  `;
+
+  const className = selected
+    ? "w-auto mx-1 font-semibold text-primary bg-primary-light focus:outline-none"
+    : "w-auto mx-1 font-normal text-gray-700 text-sm transition-colors duration-300 ease-out hover:bg-primary-light hover:text-primary";
+
+  return <ButtonContainer className={className}>{title}</ButtonContainer>;
+};
 
 interface MenuBarButtonsProps {
-  setSelectedButton: (name: string | null) => () => void;
-  selectedButton: string;
+  setSelectedButton: (name: Tag | null) => () => void;
+  selectedButton: Tag;
 }
 
 export const MenuBarButtons: React.FunctionComponent = (props: MenuBarButtonsProps) => {
-  const onSelectFilter = (name: string | null) => () => {
+  const onSelectFilter = (name: Tag | null) => () => {
     props.setSelectedButton(name);
-  };
-
-  const renderButton = (buttonName: string | null): JSX.Element => {
-    if (!buttonName) {
-      return !props.selectedButton ? (
-        <ButtonContainer className="w-auto mx-1 font-semibold text-primary bg-primary-light focus:outline-none">
-          All
-        </ButtonContainer>
-      ) : (
-        <ButtonContainer
-          className="w-auto mx-1 font-normal text-gray-700 text-sm transition-colors duration-300 ease-out
-              hover:bg-primary-light hover:text-primary"
-        >
-          All
-        </ButtonContainer>
-      );
-    }
-
-    return props.selectedButton === buttonName ? (
-      <ButtonContainer className="w-auto mx-1 font-semibold text-primary bg-primary-light focus:outline-none">
-        {buttonName}
-      </ButtonContainer>
-    ) : (
-      <ButtonContainer
-        className="w-auto mx-1 font-normal text-gray-700 text-sm transition-colors duration-300 ease-out
-        hover:bg-primary-light hover:text-primary"
-      >
-        {buttonName}
-      </ButtonContainer>
-    );
   };
 
   return (
     <div className="flex flex-wrap">
-      <div onClick={onSelectFilter(null)}>{renderButton(null)}</div>
-      <div onClick={onSelectFilter(Tag.TRADETRUST)}>{renderButton("TradeTrust")}</div>
-      <div onClick={onSelectFilter(Tag.OPENCERTS)}>{renderButton("Opencerts")}</div>
-      <div onClick={onSelectFilter(Tag.LICENCE)}>{renderButton("Licence")}</div>
+      <div onClick={onSelectFilter(null)}>
+        <Button title="All" selected={props.selectedButton === null} />
+      </div>
+      <div onClick={onSelectFilter(Tag.TRADETRUST)}>
+        <Button title="TradeTrust" selected={props.selectedButton === Tag.TRADETRUST} />
+      </div>
+      <div onClick={onSelectFilter(Tag.OPENCERTS)}>
+        <Button title="Opencerts" selected={props.selectedButton === Tag.OPENCERTS} />
+      </div>
+      <div onClick={onSelectFilter(Tag.LICENCE)}>
+        <Button title="Licence" selected={props.selectedButton === Tag.LICENCE} />
+      </div>
     </div>
   );
 };
