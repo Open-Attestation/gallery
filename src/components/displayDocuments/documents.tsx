9b57451515bf23d92f01a9457742979d369e4d99
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-02-19 16:45:22 +0800

    feat: add url to action

--- src/components/displayDocuments/documents.tsx
+++ src/components/displayDocuments/documents.tsx
@@ -5,6 +5,10 @@ export interface Document {
   tags: Tag[];
 }
 
+const urlToAction = (url: string, key: string, permittedActions: string, redirect: string): string => {
+  return `https://action.openattestation.com/?q=%7B%22type%22:%22DOCUMENT%22,%22payload%22:%7B%22uri%22:%22${url}%22,%22key%22:%22${key}%22,%22permittedActions%22:${permittedActions},%22redirect%22:%22${redirect}%22%7D%7D`;
+};
+
 export enum Tag {
   TRADETRUST = "TradeTrust",
   OPENCERTS = "OpenCerts",
@@ -14,8 +18,12 @@ export enum Tag {
 export const documents: Document[] = [
   {
     title: "OpenCerts Demo",
-    url:
-      "https://action.openattestation.com?q=%7B%22type%22:%22DOCUMENT%22,%22payload%22:%7B%22uri%22:%22https://api.myjson.com/bins/1a9acm%22,%22key%22:%221b8c334a38f9ff96108303a4ba0cc592f1559eb24f5b48b70c9300c60a34d5e9%22,%22permittedActions%22:%5B%22STORE%22%5D,%22redirect%22:%22https://dev.opencerts.io%22%7D%7D",
+    url: urlToAction(
+      "https://api.myjson.com/bins/1a9acm",
+      "1b8c334a38f9ff96108303a4ba0cc592f1559eb24f5b48b70c9300c60a34d5e9",
+      `["STORE"]`,
+      "https://dev.opencerts.io"
+    ),
     imageName: "opencerts-demo",
     tags: [Tag.OPENCERTS]
   },
