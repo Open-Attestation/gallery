d43f6e9eea7964f1ac885083bcd3c0213d02de97
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2020-02-24 18:40:21 +0800

    feat: add documents (#33)

--- src/components/displayDocuments/documents.tsx
+++ src/components/displayDocuments/documents.tsx
@@ -7,14 +7,14 @@ export interface Document {
 
 interface Action {
   uri: string;
-  key: string;
-  permittedActions: string;
+  key?: string;
+  permittedActions: string[];
   redirect: string;
 }
 
 const uriToAction = ({ uri, key, permittedActions, redirect }: Action): string => {
   return (
-    "https://action.openattestation.com/?q=" +
+    "https://action.openattestation.com?q=" +
     window.encodeURI(
       JSON.stringify({
         type: "DOCUMENT",
@@ -43,40 +43,30 @@ export const documents: Document[] = [
     uri: uriToAction({
       uri: "https://api.myjson.com/bins/1a9acm",
       key: "1b8c334a38f9ff96108303a4ba0cc592f1559eb24f5b48b70c9300c60a34d5e9",
-      permittedActions: `["STORE"]`,
+      permittedActions: ["STORE"],
       redirect: "https://dev.opencerts.io"
     }),
     imageSrc: "/static/img/opencerts-demo.jpg",
-    tags: [Tag.OPENCERTS]
+    tags: [Tag.OPENCERTS, Tag.STORABLE]
   },
   {
-    title: "Degree Certificate with Transcript",
-    uri: "",
-    imageSrc: "/static/img/opencerts-demo.jpg",
+    title: "TradeTrust eBL",
+    uri: uriToAction({
+      uri: "https://api.myjson.com/bins/kv1de",
+      permittedActions: ["VIEW", "STORE"],
+      redirect: "https://dev.tradetrust.io"
+    }),
+    imageSrc: "/static/img/tradetrust-ebl.png",
     tags: [Tag.TRADETRUST]
   },
   {
-    title: "Certificate of Non-manipulation",
-    uri: "",
-    imageSrc: "/static/img/opencerts-demo.jpg",
-    tags: [Tag.TRADETRUST, Tag.STORABLE]
-  },
-  {
-    title: "Ngee Ann Polytechnic",
-    uri: "",
-    imageSrc: "/static/img/opencerts-demo.jpg",
-    tags: [Tag.TRADETRUST, Tag.STORABLE]
-  },
-  {
-    title: "MPA Certificate of Competency",
-    uri: "",
-    imageSrc: "/static/img/opencerts-demo.jpg",
-    tags: [Tag.TRADETRUST]
-  },
-  {
-    title: "ACRA BizFile+",
-    uri: "",
-    imageSrc: "/static/img/opencerts-demo.jpg",
-    tags: [Tag.TRADETRUST]
+    title: "CAAS UAPL",
+    uri: uriToAction({
+      uri: "https://api.myjson.com/bins/95dti",
+      permittedActions: ["VIEW", "STORE"],
+      redirect: "https://dev.opencerts.io"
+    }),
+    imageSrc: "/static/img/cass-uapl.png",
+    tags: [Tag.OPENCERTS, Tag.STORABLE]
   }
 ];
