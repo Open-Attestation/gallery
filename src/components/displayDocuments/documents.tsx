e08958da8d3747dc0a2b762849132b8665d73431
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-30 12:38:56 +0800

    fix: update cert to encypted ver

--- src/components/displayDocuments/documents.tsx
+++ src/components/displayDocuments/documents.tsx
@@ -41,7 +41,8 @@ export const documents: Document[] = [
   {
     title: "OpenCerts Demo",
     uri: uriToAction({
-      uri: "https://dev.opencerts.io/static/demo/ropsten.opencert",
+      uri: "https://raw.githubusercontent.com/gjj/opencerts-certificates/master/ropsten-encrypted.opencert",
+      key: "5b433c297f3b35690461b9ee08d77f3e8ee47ec86e5b8b1322b056da6f0b86c4",
       permittedActions: ["STORE"],
       redirect: "https://dev.opencerts.io"
     }),
