60b40ff9c9c15178f6661737c9e5328714e7eb8b
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-08-05 13:40:37 +0800

    feat: add google analytics via vite plugin radar
    https://github.com/stafyniaksacha/vite-plugin-radar?tab=readme-ov-file#add-it-to-viteconfigjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginRadar({ analytics: { id: "G-MQ25WGT4TM" } })],
   build: {
     outDir: "out",
     emptyOutDir: true,
